80ee9957f432d853f7e366f6407d2b5b118418e2
Jake <114455978+jlorenzen024@users.noreply.github.com>
2023-01-17 00:31:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function clickHandle1() {
+    window.location.href = "/jacksonville.html";
+}
+
+function clickHandle2() {
+    window.location.href = "/josephine.html";
+}
+
+function clickHandle3() {
+    window.location.href = "/index.html"
+}
